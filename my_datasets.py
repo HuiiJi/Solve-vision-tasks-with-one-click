9ccf17c2d43db4cfffcc3d2048cc5f6974b8d130
Huii <72867414+752413464@users.noreply.github.com>
2022-02-06 12:44:01 +0800

    Update my_datasets.py

--- my_datasets.py
+++ my_datasets.py
@@ -178,7 +178,7 @@ class LoadImages:  # for inference
             # Read image
             self.count += 1
             path = path.split('\\')[-1]
-            img0 = cv2.imread(f'images/' + path )  # BGR
+            img0 = cv2.imread(f'data/images/' + path )  # BGR
             assert img0 is not None, 'Image Not Found ' + path
             # print(f'image {self.count}/{self.nf} {path}: ', end='')
 
