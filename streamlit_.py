c0940b8d5bf25627614aac2a314286c1f0572e78
Huii <72867414+752413464@users.noreply.github.com>
2022-02-10 19:46:44 +0800

    Update streamlit_.py

--- streamlit_.py
+++ streamlit_.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
                         file_name='clean.avi',
                     )
                 with st.spinner(text='Preparing Images'):
-                    st.video('runs/detect/clean.avi')
+                    st.video('runs/detect/clean.avi', format='avi', start_time=0)
 
 
         if option == "视觉去噪":
@@ -112,7 +112,7 @@ if __name__ == '__main__':
                     )
 
                 with st.spinner(text='Preparing Video'):
-                        st.video('runs/detect/clean.avi',  format='mp4', start_time=0)
+                        st.video('runs/detect/clean.avi',  format='avi', start_time=0)
 
 
         if option == "视觉去雾":
@@ -137,7 +137,7 @@ if __name__ == '__main__':
                         )
 
                 with st.spinner(text='Preparing Video'):
-                        st.video('runs/detect/clean.mp4',  format='avi', start_time=0)
+                        st.video('runs/detect/clean.avi',  format='avi', start_time=0)
                    
               
 
