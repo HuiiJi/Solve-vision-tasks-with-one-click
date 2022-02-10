c8c3f0957ba270272728050af98b49c386b2fad4
Huii <72867414+752413464@users.noreply.github.com>
2022-02-10 16:22:42 +0800

    Update my_datasets.py

--- my_datasets.py
+++ my_datasets.py
@@ -7,6 +7,7 @@ import os
 import random
 import shutil
 import time
+from PIL import Image
 from itertools import repeat
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
@@ -121,31 +122,28 @@ class _RepeatSampler(object):
 class LoadImages:  # for inference
     def __init__(self, path, img_size=640, stride=32):
         p = str(Path(path).absolute())  # os-agnostic absolute path
-        if '*' in p:
-            files = sorted(glob.glob(p, recursive=True))  # glob
-        elif os.path.isdir(p):
-            files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
-        elif os.path.isfile(p):
-            files = [p]  # files
-        else:
-            raise Exception(f'ERROR: {p} does not exist')
-
+        files = [p]
         images = [x for x in files if x.split('.')[-1].lower() in img_formats]
         videos = [x for x in files if x.split('.')[-1].lower() in vid_formats]
         ni, nv = len(images), len(videos)
+
+        if any(videos):
+            self.mode = 'video'
+        if any(images):
+            self.mode = 'image'
 
         self.img_size = img_size
         self.stride = stride
         self.files = images + videos
         self.nf = ni + nv  # number of files
         self.video_flag = [False] * ni + [True] * nv
-        self.mode = 'image'
-        if any(videos):
-            self.new_video(videos[0])  # new video
-        else:
-            self.cap = None
-        assert self.nf > 0, f'No images or videos found in {p}. ' \
-                            f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
+
+        # if any(videos):
+        #     self.new_video(videos[0])  # new video
+        # else:
+        #     self.cap = None
+        # assert self.nf > 0, f'No images or videos found in {p}. ' \
+        #                     f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
 
     def __iter__(self):
         self.count = 0
@@ -155,47 +153,21 @@ class LoadImages:  # for inference
         if self.count == self.nf:
             raise StopIteration
         path = self.files[self.count]
-
-
-        if self.video_flag[self.count]:
-            # Read video
-            self.mode = 'video'
-            ret_val, img0 = self.cap.read()
-            if not ret_val:
-                self.count += 1
-                self.cap.release()
-                if self.count == self.nf:  # last video
-                    raise StopIteration
-                else:
-                    path = self.files[self.count]
-                    self.new_video(path)
-                    ret_val, img0 = self.cap.read()
-
-            self.frame += 1
-            print(f'video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ', end='')
-
-        else:
-            # Read image
+        if self.mode == 'video':
+            path = path.split('\\')[-1]
+            self.cap = cv2.VideoCapture(f'data/videos/' + path)
+            img0 = None
             self.count += 1
-#             path = path.split('\\')[-1]
-            img0 = cv2.imread(path)  # BGR
-            assert img0 is not None, 'Image Not Found ' + path
-            # print(f'image {self.count}/{self.nf} {path}: ', end='')
+        else:
+            self.cap = None
+            self.count += 1
+            path = path.split('\\')[-1]
+            img0 = cv2.imread('data/images/' + path)  # BGR
+            img0 = cv2.resize(img0 ,(int( img0.shape[1] / 2) , int (img0.shape[0]/2)))
+            img0 = img0[:, :, ::-1].transpose(2, 0, 1)
+            img0 = np.ascontiguousarray(img0)
 
-        # Padded resize
-        img = letterbox(img0, self.img_size, stride=self.stride)[0]
-
-        # Convert
-#         img = cv2.resize(img, (512, 512))
-        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-        img = np.ascontiguousarray(img)
-
-        return path, img, img0, self.cap
-
-    def new_video(self, path):
-        self.frame = 0
-        self.cap = cv2.VideoCapture(path)
-        self.nframes = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        return path, img0, self.cap, self.mode
 
     def __len__(self):
         return self.nf  # number of files
@@ -248,16 +220,13 @@ class LoadWebcam:  # for inference
 
         # Padded resize
         img = letterbox(img0, self.img_size, stride=self.stride)[0]
-
         # Convert
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
         img = np.ascontiguousarray(img)
-
         return img_path, img, img0, None
 
     def __len__(self):
         return 0
-
 
 class LoadStreams:  # multiple IP or RTSP cameras
     def __init__(self, sources='streams.txt', img_size=640, stride=32):
@@ -287,7 +256,6 @@ class LoadStreams:  # multiple IP or RTSP cameras
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             self.fps = cap.get(cv2.CAP_PROP_FPS) % 100
-
             _, self.imgs[i] = cap.read()  # guarantee first frame
             thread = Thread(target=self.update, args=([i, cap]), daemon=True)
             print(f' success ({w}x{h} at {self.fps:.2f} FPS).')
@@ -295,10 +263,10 @@ class LoadStreams:  # multiple IP or RTSP cameras
         print('')  # newline
 
         # check for common shapes
-        s = np.stack([letterbox(x, self.img_size, stride=self.stride)[0].shape for x in self.imgs], 0)  # shapes
-        self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
-        if not self.rect:
-            print('WARNING: Different stream shapes detected. For optimal performance supply similarly-shaped streams.')
+        # s = np.stack([letterbox(x, self.img_size, stride=self.stride)[0].shape for x in self.imgs], 0)  # shapes
+        # self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
+        # if not self.rect:
+        #     print('WARNING: Different stream shapes detected. For optimal performance supply similarly-shaped streams.')
 
     def update(self, index, cap):
         # Read next stream frame in a daemon thread
@@ -816,7 +784,6 @@ def replicate(img, labels):
 
     return img, labels
 
-
 def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
     shape = img.shape[:2]  # current shape [height, width]
@@ -824,7 +791,8 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
         new_shape = (new_shape, new_shape)
 
     # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1]) #min(640/544, 640/960) = 0
+
     if not scaleup:  # only scale down, do not scale up (for better test mAP)
         r = min(r, 1.0)
 
