e26d636e33349d5e04db8fa3b0c056d13dd78062
Huii <72867414+752413464@users.noreply.github.com>
2022-02-10 15:46:44 +0800

    Update detect_DMSHN.py

--- detect_DMSHN.py
+++ detect_DMSHN.py
@@ -2,72 +2,93 @@ import argparse
 from pathlib import Path
 import os
 import cv2
+import torchvision
 from PIL import Image
 import numpy as np
 from DMSHNet import DMSHN
+from FFANet import FFA
 from torch.utils.data import DataLoader
 import torch
 import torch.backends.cudnn as cudnn
-from my_datasets import  LoadImages
+from my_datasets import  LoadImages , letterbox
 from my_general import check_img_size, check_requirements, increment_path
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--source', type=str,
-                    default='images', help='source')
-parser.add_argument('--img-size', type=int, default=640,
-                    help='inference size (pixels)')
-parser.add_argument('--augment', action='store_true',
-                    help='augmented inference')
-parser.add_argument('--update', action='store_true',
+                    default='', help='source')
+parser.add_argument('--task', type = str, default ='',
                     help='update all models')
-parser.add_argument('--task', type = str, default = '',
-                    help='update all models')
-parser.add_argument('--project', default='runs/detect',
-                    help='save results to project/name')
-parser.add_argument('--name', default='exp',
-                    help='save results to project/name')
-parser.add_argument('--exist-ok', action='store_true',
-                    help='existing project/name ok, do not increment')
 opt = parser.parse_args()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 device_ids = [Id for Id in range(torch.cuda.device_count())]
 
-DMSHN = DMSHN().to(device)
-DMSHN= torch.nn.DataParallel(DMSHN, device_ids=device_ids)
+FFA = FFA().to(device)
+FFA = torch.nn.DataParallel(FFA, device_ids=device_ids)
 
-save_dir = Path(increment_path(Path(opt.project) / opt.name,
-                               exist_ok=opt.exist_ok))  # increment run
-def detect(opt , task):
-    source, imgsz = opt.source, opt.img_size
+def detect(opt , task ):
+
+    source = opt.source
     if task == 'derain':
-        DMSHN.load_state_dict(torch.load('DMSHN_derain.pth',  map_location='cpu'))
+        FFA.load_state_dict(torch.load('snapshots/FFA_derain.pth' ,  map_location = 'cpu'))
     if task == 'denoisy':
-        DMSHN.load_state_dict(torch.load('DMSHN_denoisy.pth',  map_location='cpu'))
+        FFA.load_state_dict(torch.load('snapshots/FFA_denoisy.pth' , map_location = 'cpu'))
     if task == 'dehaze':
-        DMSHN.load_state_dict(torch.load('DMSHN_dehaze.pth',  map_location='cpu'))
-    stride = 32
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+        FFA.load_state_dict(torch.load('snapshots/FFA_dehaze.pth' ,map_location = 'cpu'))
+    dataset = LoadImages(source, img_size=640, stride=32)
+
 
     with torch.no_grad():
-        DMSHN.eval()
-        for path, img, im0s, vid_cap in dataset:
-            img = torch.from_numpy(img).float()
-            img = img.to(device)
-            img /= 255.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
+        FFA.eval()
+        for path, img0, cap, mode in dataset:
+           if mode == 'video':
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                out = cv2.VideoWriter('runs/detect/clean.mp4', cv2.VideoWriter_fourcc('X','2','6','4'), fps, (int(w/2), int(h/2)), True)
+                while (cap.isOpened()):
+                    ret_val, img = cap.read()
+                    if ret_val :
+                        img = cv2.resize(img, (int(w/2), int(h/2)))
+                        img = img[:, :, ::-1].transpose(2, 0, 1)
+                        img = np.ascontiguousarray(img)
+                        img = torch.from_numpy(img).float()
+                        img /= 255
+                        img = img.to(device)
+                        if img.ndimension() == 3:
+                            img = img.unsqueeze(0)
+                        clean, _ = FFA(img)
+                        clean = torch.tensor(clean * 255 , dtype=torch.uint8)
+                        clean = clean.cpu().numpy()
+                        clean= clean.squeeze(0).transpose(1, 2, 0)
+                        clean = clean[:, :, ::-1]
+                        # clean = cv2.resize(clean, (256, 256))
 
-            clean, _ = DMSHN(img)
-            clean = clean.cpu().numpy()
-            clean = clean.squeeze(0).transpose(1, 2, 0)
+                        out.write(clean)
+                    else:
+                        break
+                cap.release()
+                out.release()
+           else:
+                for path, img0, cap, mode in dataset:
 
-            clean = clean * 255
-            clean = clean[:, :, ::-1]
-            cv2.imwrite(str(save_dir) + '.jpg', clean)
+                    img0 = torch.from_numpy(img0).float() /255
+                    img0 = img0.to(device)
+                    if img0.ndimension() == 3:
+                        img0 = img0.unsqueeze(0)
+                    clean, _ = FFA(img0)
+                    torchvision.utils.save_image(clean, 'runs/detect/clean.jpg')
+
+                    # clean = clean.cpu().numpy()
+                    # clean = clean.squeeze(0).transpose(1, 2, 0)
+                    # clean = clean * 255
+                    # clean = clean[:, :, ::-1]
+                    # cv2.imwrite(f'runs/detect/clean.jpg', clean)
+
+
+
+
 
 if __name__ == '__main__':
-    # check_requirements(exclude=('pycocotools', 'thop'))
     detect(opt, opt.task)
