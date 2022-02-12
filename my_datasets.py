d59b625c5177c8c997fe7ab3ea62b9bc5348ab36
Huii <72867414+752413464@users.noreply.github.com>
2022-02-12 13:03:09 +0800

    Update my_datasets.py

--- my_datasets.py
+++ my_datasets.py
@@ -1,34 +1,9 @@
-# Dataset utils and dataloaders
 
-import glob
-import logging
-import math
-import os
-import random
-import shutil
-import time
-from itertools import repeat
-from multiprocessing.pool import ThreadPool
 from pathlib import Path
-from threading import Thread
-
 import cv2
-import numpy as np
-import torch
-import torch.nn.functional as F
-from PIL import Image, ExifTags
-from torch.utils.data import Dataset
-from tqdm import tqdm
 
-
-
-# Parameters
-help_url = 'https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data'
 img_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp', 'mpo']  # acceptable image suffixes
 vid_formats = ['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv']  # acceptable video suffixes
-logger = logging.getLogger(__name__)
-
-
 
 
 class LoadImages:  # for inference
@@ -38,22 +13,13 @@ class LoadImages:  # for inference
         images = [x for x in files if x.split('.')[-1].lower() in img_formats]
         videos = [x for x in files if x.split('.')[-1].lower() in vid_formats]
         ni, nv = len(images), len(videos)
-
         if any(videos):
             self.mode = 'video'
         if any(images):
             self.mode = 'image'
-            
         self.files = images + videos
         self.nf = ni + nv  # number of files
         self.video_flag = [False] * ni + [True] * nv
-
-        # if any(videos):
-        #     self.new_video(videos[0])  # new video
-        # else:
-        #     self.cap = None
-        # assert self.nf > 0, f'No images or videos found in {p}. ' \
-        #                     f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
 
     def __iter__(self):
         self.count = 0
@@ -64,16 +30,18 @@ class LoadImages:  # for inference
             raise StopIteration
         path = self.files[self.count]
         if self.mode == 'video':
-            self.cap = cv2.VideoCapture(path)
+            path = path.split('\\')[-1]
+            self.cap = cv2.VideoCapture(f'data/videos/' + path)
             img0 = None
             self.count += 1
         else:
             self.cap = None
             self.count += 1
-            img0 = cv2.imread (path)  # BGR
-            img0 = img0[:, :, ::-1].transpose(2, 0, 1)
-            img0 = np.ascontiguousarray(img0)
+            path = path.split('\\')[-1]
+            img0 = cv2.imread('data/images/' + path)  # BGR
         return path, img0, self.cap, self.mode
 
     def __len__(self):
         return self.nf  # number of files
+
+
